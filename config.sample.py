ec0fcbcc760829e0d47ecb140b1a68c7a8f14f73
Matt Brooks <mbrooks34@gsu.edu>
2022-03-28 15:45:58 -0400

    In progress.

---
+++ config.sample.py
@@ -0,0 +1,6 @@
+url = 'https://api.datacite.org/dois'
+encoding = 'utf-8' # https://docs.python.org/3/library/codecs.html#standard-encodings
+cilentId = ''
+doiPrefix = ''
+username = ''
+password = ''
