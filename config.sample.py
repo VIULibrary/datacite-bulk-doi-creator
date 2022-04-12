dfa009adfa68e3251ae43664e977eef594a01d9a
Matt Brooks <mbrooks34@gsu.edu>
2022-04-12 10:24:39 -0400

    Encoding no longer needed.

--- config.sample.py
+++ config.sample.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 url = 'https://api.datacite.org/dois'
-encoding = 'utf-8' # https://docs.python.org/3/library/codecs.html#standard-encodings
 doiPrefix = ''
 username = ''
 password = ''
